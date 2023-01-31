0a20242a188729bce81593f809c0d9d461ce2b39
poppy04 <115389708+poppy04@users.noreply.github.com>
2023-01-31 21:52:06 +0900

    아무것도 안함
    helloworld

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class NewBehaviourScript : MonoBehaviour
+	{
+		[SerializeField]
+		private string helloText = "안녕";
+
+		[SerializeField] private bool isDie;
+	}
